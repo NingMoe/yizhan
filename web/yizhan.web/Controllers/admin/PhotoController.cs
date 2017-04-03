fbc9ec15324a2898726924904b7947e9ba5e456a
yuchao <305694029@qq.com>
2017-04-03 15:41:49 +0800

    创建项目

---
+++ web/yizhan.web/Controllers/admin/PhotoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yizhan.web.Controllers.admin
+{
+    public class PhotoController : Controller
+    {
+        
+    }
+}
---
+++ web/yizhan.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yizhan.web.MvcApplication" Language="C#" %>
