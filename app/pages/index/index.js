a51f65143297c36f0a9263fa536109effffb8544
yuchao <305694029@qq.com>
2017-04-05 22:16:22 +0800

    重新导入app

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,18 +1,7 @@
-import Util from 'Util';  
-  
+ 
 Page({  
   data:{  
-        imageWidth:0,  
-        imageHeight:0  
+        
   },  
-  imageLoad: function (e) {    
-        //获取图片的原始宽度和高度  
-        let originalWidth = e.detail.width;  
-        let originalHeight = e.detail.height;  
-        let imageSize = Util.imageZoomHeightUtil(originalWidth,originalHeight);  
-        //let imageSize = Util.imageZoomHeightUtil(originalWidth,originalHeight,375);  
-        //let imageSize = Util.imageZoomWidthUtil(originalWidth,originalHeight,145);  
   
-        this.setData({imageWidth:imageSize.imageWidth,imageHeight:imageSize.imageHeight});    
-  }  
 })   
--- app/pages/pics1/pics1.js
+++ app/pages/pics1/pics1.js
@@ -1,32 +1,9 @@
-import Util from 'Util';  
   
 Page({  
   data:{  
-        imageWidth:0,  
-        imageHeight:0, 
-        imageWidth2:0,  
-        imageHeight2:0 
+        
   },  
-  imageLoad: function (e) {    
-        //获取图片的原始宽度和高度  
-        let originalWidth = e.detail.width;  
-        let originalHeight = e.detail.height;  
-        let imageSize = Util.imageZoomHeightUtil(originalWidth,originalHeight);  
-        //let imageSize = Util.imageZoomHeightUtil(originalWidth,originalHeight,375);  
-        //let imageSize = Util.imageZoomWidthUtil(originalWidth,originalHeight,145);  
   
-        this.setData({imageWidth:imageSize.imageWidth,imageHeight:imageSize.imageHeight});    
-  },  
-  imgl: function (e) {    
-        //获取图片的原始宽度和高度  
-        let originalWidth2 = e.detail.width;  
-        let originalHeight2 = e.detail.height;  
-        let imageSize2 = Util.imageZoomHeightUtil(originalWidth2,originalHeight2);  
-        //let imageSize = Util.imageZoomHeightUtil(originalWidth,originalHeight,375);  
-        //let imageSize = Util.imageZoomWidthUtil(originalWidth,originalHeight,145);  
-  
-        this.setData({imageWidth2:imageSize2.imageWidth,imageHeight2:imageSize2.imageHeight});    
-  }
     
   
   
